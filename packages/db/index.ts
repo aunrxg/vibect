fb4219044d433eb4f3ea85fece3ad717dcd0aa50
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-11-01 13:13:20 +0530

    feat(db): added new db package (no schemas)

---
+++ packages/db/index.ts
@@ -0,0 +1,14 @@
+export * from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+// Prevent multiple instances of Prisma Client in development
+
+export const prisma = globalThis.prisma ?? new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : ["warn", "error"],
+});
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
