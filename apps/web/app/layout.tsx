6289fc0fa32e3c5cab8a500df19d0a56191b6999
Anurag Poddar <anuragpoddar9484@gmail.com>
2025-11-29 00:33:54 +0530

    fix(ui): fix dark/light theme and add meta data to site

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,6 +16,37 @@ export const metadata: Metadata = {
   title: "Vibect - Collaborative music streaming",
   description:
     "Create spaces where viewers can add songs, vote, and enjoy music together in real-time",
+  creator: "Anurag Poddar",
+  openGraph: {
+    title: "Vibect - Collaborative music streaming",
+    description:
+      "Create spaces where viewers can add songs, vote, and enjoy music together in real-time",
+    url: "http://localhost:3000",
+    siteName: "Vibect",
+    images: [
+      {
+        url: "/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "MusicSpace",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vibect - Collaborative music streaming",
+    description:
+      "Create spaces where viewers can add songs, vote, and enjoy music together in real-time",
+    images: ["/og_image.png"],
+    creator: "@aunrxg",
+  },
+  // icons: {
+  //   icon: "",
+  //   shortcut: "",
+  //   apple: "",
+  // },
 };
 
 export default function RootLayout({
@@ -23,9 +55,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
